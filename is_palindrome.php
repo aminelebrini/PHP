18d3d2a02e2cec0dcde48de44eb487c15e8e1ebc
aminelebrini <aminelebrini4@gmail.com>
2025-11-26 12:48:34 +0100

    verifier le string si est palindrome ou non

--- is_palindrome.php
+++ is_palindrome.php
@@ -5,12 +5,13 @@
     for($x = 0; $x < $length; $x++)
     {
         $rev = $str[$length - 1 - $x];
-        echo $rev;
-        if($rev === $str)
-        {
-            echo "EST PALINDROME";
-        }else{
-            echo "N'EST PAS UNE PALINDROME";
-        }
+         echo $rev;
+    }
+    echo "\n";
+    if($rev === $str)
+    {
+        echo "EST PALINDROME";
+    }else{
+        echo "N'EST PAS UNE PALINDROME";
     }
 ?>
