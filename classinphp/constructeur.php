97ca4bd8c322446818b0a263a1d70b83d95d34e2
aminelebrini <aminelebrini4@gmail.com>
2025-12-12 12:08:26 +0100

    constructeur en utilisant __construct

---
+++ classinphp/constructeur.php
@@ -0,0 +1,15 @@
+<?php
+    class person{
+        public $name;
+
+        function __construct($name)
+        {
+            echo "Bonjour " . $this->name = $name;
+        }
+    }
+
+    $per = new person("amine");
+
+    $per->name;
+
+?>
