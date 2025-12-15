3942bd67ec1a0ea137409eba09313b6043dd8ca9
aminelebrini <aminelebrini4@gmail.com>
2025-12-15 13:42:31 +0100

    connexion en utilisant php PDO

---
+++ connectwithdb/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB</title>
+</head>
+<body>
+   <body>
+    <?php 
+    $servername = "localhost";
+    $username   = "root";
+    $serverpass = "123456";
+    $dbname     = "db_test";
+
+try {
+    $connection = new PDO(
+        "mysql:host=$servername;dbname=$dbname;charset=utf8",
+        $username,
+        $serverpass
+    );
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion OK";
+    } catch (PDOException $e) {
+        echo "Erreur de connexion";
+    }
+
+?>
+
+</body>
+</body>
+</html>
